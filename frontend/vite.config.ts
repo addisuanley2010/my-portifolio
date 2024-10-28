22a74fdcebafefe22c415f747983c89bc265b0b1
addisuanley2010 <addisuanley2005@gmail.com>
2024-10-28 17:11:17 +0300

    last chance

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+    base: './',
    css: {
     postcss: './postcss.config.js',
   },
 })
+// export default {
+//   base: './',
+// };
